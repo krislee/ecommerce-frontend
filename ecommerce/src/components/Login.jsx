d8e63c5e06960c2ce8305c6f7090f8814ff1bf44
Kwok <kwokrenx@gmail.com>
2021-01-09 14:12:59 -0500

    testing on change for the input fields

--- ecommerce/src/components/Login.jsx
+++ ecommerce/src/components/Login.jsx
@@ -7,16 +7,38 @@ import '../styles/BuyerLogin.css'
 
 function Login ({}) {
 
-    const fetchItems = () => {
-        fetch()
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const testChange = () => {
+        console.log(username);
+        console.log(password);
+        console.log(email);
     }
 
+    const handleChangeUsername = e => {
+        setUsername({ [e.target.name]: e.target.value })
+    }
+
+    const handleChangePassword = e => {
+        setPassword({ [e.target.name]: e.target.value })
+    }
+
+    const handleChangeEmail = e => {
+        setEmail({ [e.target.name]: e.target.value })
+    }
+
+    // const fetchItems = () => {
+    //     fetch()
+    // }
+
     return (
-        <form className="login" onSubmit={fetchItems}>
-            <input type="text" placeholder="Username"></input>
-            <input type="text" placeholder="Email"></input>
-            <input type="text" placeholder="Password"></input>
-            <input className="submit-button" type="submit"></input>
+        <form className="login">
+            <input type="text" placeholder="Username" value={username} onChange={handleChangeUsername}></input>
+            <input type="text" placeholder="Email" value={email} onChange={handleChangeEmail}></input>
+            <input type="text" placeholder="Password" value={password} onChange={handleChangePassword}></input>
+            <input className="submit-button" type="submit" onSubmit={testChange()}></input>
         </form>
     )
 }
