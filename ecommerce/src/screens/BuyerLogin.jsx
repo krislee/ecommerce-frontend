818ddbbe0e09f82879deb9e8a03fa95f4e01dd26
Kwok <kwokrenx@gmail.com>
2021-01-09 13:25:56 -0500

    added back buttons to the login pages

--- ecommerce/src/screens/BuyerLogin.jsx
+++ ecommerce/src/screens/BuyerLogin.jsx
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import Input from '../components/Input'
+import Button from '../components/Button'
 
 function BuyerLogin ({}) {
     return (
         <React.Fragment>
             <Input name={'Buyer Login'}/>
+            <Link to="/">
+                <Button name={'Back'}></Button>
+            </Link>
         </React.Fragment>
     )
 }
--- ecommerce/src/screens/SellerLogin.jsx
+++ ecommerce/src/screens/SellerLogin.jsx
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import Input from '../components/Input'
+import Button from '../components/Button'
 
 function SellerLogin ({}) {
     return (
         <React.Fragment>
             <Input name={'Seller Login'}/>
+            <Link to="/">
+                <Button name={'Back'}></Button>
+            </Link>
         </React.Fragment>
     )
 }
