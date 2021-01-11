d7d2c8243b5402fb2b56e9f4f43b0d787fa6c550
Kwok <kwokrenx@gmail.com>
2021-01-11 13:08:58 -0500

    added more styles

--- ecommerce/src/components/Item.jsx
+++ ecommerce/src/components/Item.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 // import '../styles/Button.css'
 
-function Item ({ name }) {
+function Item ({ name, description}) {
     return (
         <div className="item">
             <div>{name}</div>
+            {/* <div>{description}</div> */}
         </div>
     )
 }
--- ecommerce/src/screens/Homepage.jsx
+++ ecommerce/src/screens/Homepage.jsx
@@ -2,15 +2,17 @@ import React, {useEffect, useState} from 'react'
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import Button from '../components/Button';
 import Item from '../components/Item';
+import '../styles/Homepage.css'
 
 function Homepage ({}) {
 
     const [items, setItems] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
+    const electronicURL = "http://localhost:3001/buyer/electronic"
 
     useEffect(() => {
         async function fetchItems() {
-            const resp = await fetch("http://localhost:3001/buyer/electronic");
+            const resp = await fetch(electronicURL);
             const data = await resp.json();
             console.log(data.allElectronic);
             setItems(data.allElectronic);
@@ -19,23 +21,30 @@ function Homepage ({}) {
         fetchItems();
     },[])
 
-    const itemList = items.map(item => 
+    const itemList = items.map((item, index) => 
         <Item 
-        key={item._id}
-        name={item.name}
+        key={index}
+        id={item._id}
+        name={item.Name}
+        description={item.Description}
         />
     )
 
+    console.log(items);
+
     return (
         <React.Fragment>
+            <div className="login-button">
             <Link to='/buyer'>
                 <Button name={'Buyer'}/>
             </Link>
             <Link to='/seller'>
                 <Button name={'Seller'}/>
             </Link>
-            {itemList}
-            <button onClick={() => console.log(items)}>Test</button>
+            </div>
+            <div className="itemContainer">
+                {itemList}
+            </div>
         </React.Fragment>
     )
 }
