1988493965816954c31f0b9918ce8506069d3b3e
Kwok <kwokrenx@gmail.com>
2021-01-29 16:22:39 -0500

    adding stripe payments

--- src/screens/Checkout/PaymentMethod.jsx
+++ src/screens/Checkout/PaymentMethod.jsx
@@ -1,10 +1,41 @@
 import React, { useEffect, useState } from 'react';
 
-function PaymentMethod ({ checkoutData }) {
+function PaymentMethod ({ backend, checkoutData, token }) {
+
+    useEffect(() => {
+        const fetchPaymentMethod = () => {
+            const paymentMethodResponse = await fetch(`${backend}/order/checkout/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token
+                }
+            })
+            const paymentMethodData = await paymentMethodResponse.json()
+            console.log(paymentMethodData);
+            if (!paymentMethodData) {
+                
+            }
+        }
+        fetchPaymentMethod();
+    },[])
+
+    // const handleClick = () => {
+    //     console.log(checkoutData);
+    //     if (token) {
+    //         if (localStorage.getItem('token')) {
+    //             console.log('Logged In')
+    //         } else {
+    //             console.log('Local Storage')
+    //         }
+    //     } else {
+    //         console.log('Not Logged')
+    //     }
+    // }
     
     return (
         <div>
-            <button onClick={() => console.log(checkoutData)}>Payment Method Component</button>
+            <button onClick={() => console.log('payment')}>Payment Method Component</button>
         </div>
     )
 
