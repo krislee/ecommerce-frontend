da075157166171639e97fc1360545186b729f6e5
Kristy Lee <leekristy01@gmail.com>
2021-02-14 17:31:33 -0500

    added websocket to render order details right after order confirmation

--- src/screens/Checkout/OrderComplete.jsx
+++ src/screens/Checkout/OrderComplete.jsx
@@ -1,37 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import NavBar from '../../components/NavigationBar';
 import CircularProgress from '@material-ui/core/CircularProgress';
+// import socketIOClient from "socket.io-client";
+import { io } from "socket.io-client";
 
 export default function OrderComplete({ backend, cartID }) {
  
     const [orderLoading, setOrderLoading] = useState(true)
     const [showOrderDetails, setShowOrderDetails] = useState(false)
-    const [order, setOrder] = useState({})
+    // const [order, setOrder] = useState({})
     const [orderItems, setOrderItems] = useState([])
-    const [orderShipping, setOrderShipping] = useState({})
+    const [orderShipping, setOrderShipping] = useState([])
+    const [orderShippingName, setOrderShippingName] = useState('')
     const [orderPayment, setOrderPayment] = useState({})
+    const [orderNumber, setOrderNumber] = useState('')
 
     const currentURL = window.location.href;
     const indexOfEqualSign = currentURL.split('=');
     const id=indexOfEqualSign[indexOfEqualSign.length - 1];
 
-    useEffect(() => setTimeout(() => setOrderLoading(false), 3000))
+    // const socketRef = useRef()
+    const socket = io('wss://elecommerce.herokuapp.com',  { transports: ['websocket', 'polling', 'flashsocket'] })
 
-    const handleOrderDetails = async() => {
-        const orderID = cartID || id
-        const orderResponse = await fetch(`${backend}/complete/list/orders/${orderID}`)
-        const orderData = await orderResponse.json()
-        console.log(orderData)
-        const shipping = orderData.order.Shipping.Address.split(",")
-        setOrder(orderData.order)
-        setOrderItems(orderData.order.Items)
-        setOrderShipping(shipping)
-        setOrderPayment(orderData.payment)
-        setShowOrderDetails(true)
-    }
+    useEffect(() => {
+        socket.on('completeOrder', (orderData) => {
+            console.log(orderData)
+            const shipping = orderData.order.Shipping.Address.split(",")
+
+            setOrderItems(orderData.order.Items)
+            setOrderShipping(shipping)
+            setOrderShippingName(orderData.order.Shipping.Name.replace(", ", " "))
+            setOrderNumber(orderData.order.OrderNumber)
+            setOrderPayment(orderData.payment)
+            setShowOrderDetails(true)
+            setOrderLoading(false)
+        })
+
+    })
+
+
+    // const handleOrderDetails = async() => {
+    //     const orderID = cartID || id
+    //     const orderResponse = await fetch(`${backend}/complete/list/orders/${orderID}`)
+    //     const orderData = await orderResponse.json()
+    //     console.log(orderData)
+    //     const shipping = orderData.order.Shipping.Address.split(",")
+    //     setOrder(orderData.order)
+    //     setOrderItems(orderData.order.Items)
+    //     setOrderShipping(shipping)
+    //     setOrderPayment(orderData.payment)
+    //     setShowOrderDetails(true)
+    // }
     
     if(orderLoading && cartID) {
-        console.log(33)
         return (
             <>
             <h1>Please wait while we process your payment</h1>
@@ -40,15 +61,14 @@ export default function OrderComplete({ backend, cartID }) {
         )
     } 
     else {
-        console.log(41)
         return (
         
             <>
                 <NavBar />
                 <h2>Thank you for your purchase! </h2>
-                <h3>Order #: {cartID || id}</h3>
-                { !showOrderDetails ? <button onClick={handleOrderDetails}>Order Details</button> : <button onClick={()=> setShowOrderDetails(!showOrderDetails)}>Close</button> }
-                { showOrderDetails && (
+                <h3>Order #: {orderNumber}</h3>
+                {/* { !showOrderDetails ? <button onClick={handleOrderDetails}>Order Details</button> : <button onClick={()=> setShowOrderDetails(!showOrderDetails)}>Close</button> }
+                { showOrderDetails && ( */}
                     
                 <div>
                     <h4>Items</h4>
@@ -60,7 +80,8 @@ export default function OrderComplete({ backend, cartID }) {
                     )})}
 
                     <h4>Shipping</h4>
-                    <p>{order.Shipping.Name.split(", ")[0]} {order.Shipping.Name.split(", ")[1]} </p>
+                    {/* <p>{order.Shipping.Name.split(", ")[0]} {order.Shipping.Name.split(", ")[1]} </p> */}
+                    <p>{orderShippingName}</p>
                     <p>{orderShipping[0]}</p>
                     <p>{orderShipping[1] === "null" || orderShipping[1] === "undefined" ? "" : orderShipping[1]}</p>
                     <p>{orderShipping[2]}, {orderShipping[3]} {orderShipping[4]} </p>
@@ -72,7 +93,7 @@ export default function OrderComplete({ backend, cartID }) {
                     <p>{orderPayment.billingDetails.address.line2 === "null" || orderPayment.billingDetails.address.line2 === "undefined" ? "" : orderPayment.billingDetails.address.line2}</p>
                     <p>{orderPayment.billingDetails.address.city}, {orderPayment.billingDetails.address.state} {orderPayment.billingDetails.address.postalCode}</p>
                 </div>
-                )}
+                {/* )} */}
                 
             </>
         
