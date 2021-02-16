37575c5674639bbcabe593b2e0a10cbbf8362c32
Kristy Lee <leekristy01@gmail.com>
2021-02-16 11:50:51 -0500

    paginated orders

--- src/screens/UserProfile/Orders.jsx
+++ src/screens/UserProfile/Orders.jsx
@@ -6,24 +6,50 @@ import CardHeader from '@material-ui/core/CardHeader';
 import IndividualOrder from './IndividualOrder'
 import {Link} from 'react-router-dom';
 import ConvertDate from './ConvertDate'
+import { makeStyles } from '@material-ui/core/styles';
+import { Pagination, PaginationItem } from '@material-ui/lab';
 
-export default function Orders ({ backend, loggedIn, orderData, grabOrderData, orderID, grabOrderID}) {
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+}));
 
+export default function Orders ({ backend, loggedIn, orderData, grabOrderData, orderID, grabOrderID, ordersTotal, grabOrdersTotal}) {
+    const classes = useStyles();
     // var month = dateObj.getUTCMonth() + 1; 
     // var day = dateObj.getUTCDate();
     // var year = dateObj.getUTCFullYear();
     
     const [showOrder, setShowOrder] = useState(false)
-    const [date, setDate] = useState('')
     
     console.log(22, orderData)
+
+    const handlePageOnChange = async(event, page) => {
+       
+        const nextPageOrdersResponse = await fetch(`${backend}/complete/list/orders?page=${page}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': loggedIn()
+            }
+        })
+        const nextPageOrdersData = await nextPageOrdersResponse.json();
+        console.log(nextPageOrdersData.orders);
+        grabOrderData(nextPageOrdersData.orders)
+        // grabOrdersTotal(nextPageOrdersData.totalPages)
+    }
+
     
     if(!showOrder) {
         return (
             <>
             
             {orderData.length === 0 ? <p>No purchases yet, but you could become the next owner of the latest gadget!</p> : (
-                orderData.map((order, index) => {
+                <>
+                {orderData.map((order, index) => {
                     return (
                         <>
                         {index === 0 && <h3>{ConvertDate(orderData[0].OrderDate)}</h3>}
@@ -43,7 +69,12 @@ export default function Orders ({ backend, loggedIn, orderData, grabOrderData, o
                         </Card>
                         </>
                     )
-                })
+                })}
+                <div className={classes.root}>
+                    <Pagination showFirstButton showLastButton size="large" variant="outlined" shape="rounded" count={ordersTotal} siblingCount={1} boundaryCount={2} onChange={handlePageOnChange} />
+                </div>  
+                </>
+
             )}
             
 
