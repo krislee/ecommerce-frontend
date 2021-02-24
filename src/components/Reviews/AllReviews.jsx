7938d850ee7a4a754307e8a4e1f3f31d7fe2e839
Kwok <kwokrenx@gmail.com>
2021-02-24 13:21:11 -0500

    If reviews exceeded 250 characters, then the length of the review would be shorten

--- src/components/Reviews/AllReviews.jsx
+++ src/components/Reviews/AllReviews.jsx
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    width: '100%',
     flexDirection: 'column',
     '& > * + *': {
       marginTop: theme.spacing(1),
@@ -15,11 +16,13 @@ const useStyles = makeStyles((theme) => ({
 export default function AllReviews({allReviews}) {
     
     const classes = useStyles();
-    const [reviewsLoading, setReviewsLoading] = useState(true)
+    const [reviewsLoading, setReviewsLoading] = useState(true);
+    const [showMoreComment, setShowMoreComment] =useState(false);
 
     useEffect(() => {
         if(allReviews.length) {
-            setReviewsLoading(false)
+            setReviewsLoading(false);
+            console.log(allReviews);
         }
     }, [allReviews])
 
@@ -28,13 +31,37 @@ export default function AllReviews({allReviews}) {
         <>
             <h5>Reviews</h5>
             {allReviews.length && (
-                allReviews.map((review, index) => { return (
-                    <div className={classes.root} key={index}>
-                        <h5>{review.Name}</h5>
-                        <Rating name="size-small" defaultValue={review.Rating} size="small" readOnly/>
-                        <p>{review.Comment}</p>
-                    </div>
-                )})
+                allReviews.map((review, index) => { 
+                    if (review.Comment.length > 250) {
+                        console.log(review.Comment.length);
+                        let comment = review.Comment
+                        console.log(comment.split(" ").length);
+                        console.log(comment.split(" ").splice(0, 50).join(" "));
+                        let newComment = `${comment.split(" ").splice(0, 50).join(" ")}...`
+                        return (
+                            <div className={classes.root} key={index}>
+                                <h5>{review.Name}</h5>
+                                <Rating name="size-small" defaultValue={review.Rating} size="small" readOnly/>
+                                {showMoreComment ? <div>
+                                    <p style={{width: '80%', wordWrap: "break-word"}}>{comment}</p>
+                                    <div style={{cursor: 'pointer'}} onClick={() => setShowMoreComment(false)}>Show Less...</div>
+                                </div> : 
+                                <div>
+                                    <p style={{width: '80%', wordWrap: "break-word"}}>{newComment}</p>
+                                    <div style={{cursor: 'pointer'}} onClick={() => setShowMoreComment(true)}>Show More...</div>
+                                </div>}
+                            </div>
+                        )
+                    } else {
+                        return (
+                            <div className={classes.root} key={index}>
+                                <h5>{review.Name}</h5>
+                                <Rating name="size-small" defaultValue={review.Rating} size="small" readOnly/>
+                                <p>{review.Comment}</p>
+                            </div>
+                        )
+                    };
+                })
             )}
         </>
     )
