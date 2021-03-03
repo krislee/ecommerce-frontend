b31b7959f0dcf48d18371b1c95c9752cd5266f70
Kristy Lee <leekristy01@gmail.com>
2021-03-03 17:24:38 -0500

    added error styling for registration

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -19,11 +19,15 @@ export default function Register ({backend, loggedIn, grabLoginInfo, buyer, sell
     const [usernameInvalid, setUsernameInvalid] = useState(false)
     const [passwordInvalid, setPasswordInvalid] = useState(false)
     const [emailInvalid, setEmailInvalid] = useState(false)
-
+    const [passwordInvalidInput, setPasswordInvalidInput] = useState(false)
+    const [usernameInvalidInput, setUsernameInvalidInput] = useState(false)
+    const [emailInvalidInput, setEmailInvalidInput] = useState(false)
     const [disabled, setDisabled] = useState(false)
+
     const handleRegister = async (event) => {
         event.preventDefault();
         setDisabled(true)
+
         const registerInfo = {
             username: username,
             password: password,
@@ -45,12 +49,16 @@ export default function Register ({backend, loggedIn, grabLoginInfo, buyer, sell
                 setEmailError(true)
             } else if(registerData.usernameMsg) {
                 setUsernameError(true)
+                setUsernameInvalidInput(true)
             } else if(registerData.details && registerData.details[0].message === "\"username\" length must be at least 8 characters long") {
                 setUsernameInvalid(true)
+                setUsernameInvalidInput(true)
             } else if(registerData.details && registerData.details[0].message === "\"password\" length must be at least 8 characters long") {
                 setPasswordInvalid(true)
+                setPasswordInvalidInput(true)
             } else if(registerData.details && registerData.details[0].message === "\"email\" must be a valid email") {
                 setEmailInvalid(true)
+                setEmailInvalidInput(true)
             }
             if (registerData.success === true) {
                 localStorage.setItem('token', registerData.token)
@@ -72,17 +80,16 @@ export default function Register ({backend, loggedIn, grabLoginInfo, buyer, sell
      /* ------- UPDATE INPUT STATES ------- */
     const handleChangeUsername = e => {
         setUsername(e.target.value)
-        // setDisabled(false)
+        setUsernameInvalidInput(false)
     }
-
     const handleChangePassword = e => {
         setPassword(e.target.value)
-        // setDisabled(false)
+        setPasswordInvalidInput(false)
     }
 
     const handleChangeEmail = e => {
         setEmail(e.target.value)
-        // setDisabled(false)
+        setEmailInvalidInput(false)
     }
 
     const handleChangeName = (e) => {
@@ -90,7 +97,6 @@ export default function Register ({backend, loggedIn, grabLoginInfo, buyer, sell
         setFullName((prevName) => ({
             ...prevName, [name]: value
         }))
-        // setDisabled(false)
     }
 
     
@@ -161,11 +167,11 @@ export default function Register ({backend, loggedIn, grabLoginInfo, buyer, sell
 
                 <input  className="register-input" type="text" placeholder="Last Name" name ="lastName" value={fullName.lastName || ""} onChange={handleChangeName}></input>
 
-                <input className="register-input" type="text" placeholder="Username" value={username} onChange={handleChangeUsername}></input>
+                <input className={usernameInvalidInput || usernameError? "register-invalid-username-input" : "register-input"} type="text" placeholder="Username" value={username} onChange={handleChangeUsername}></input>
 
-                <input className="register-input" type="email" placeholder="Email" value={email} onChange={handleChangeEmail}></input>
+                <input className={emailInvalidInput || emailError ? "register-invalid-email-input" : "register-input"} type="email" placeholder="Email" value={email} onChange={handleChangeEmail}></input>
 
-                <input className="register-input" type="password" placeholder="Password" value={password} onChange={handleChangePassword}  autoComplete="current-password"></input>
+                <input className={passwordInvalidInput ? "register-invalid-password-input" : "register-input"} type="password" placeholder="Password" value={password} onChange={handleChangePassword}  autoComplete="current-password"></input>
 
                 {(email === '' || fullName.firstName === '' || fullName.lastName === '' || password === '' || username === '' || disabled) ? <Button id="submit-button-disabled" type="submit" variant="dark" size='lg' disabled={true}>Submit</Button> :  <Button size='lg' id="submit-button" variant="dark" type="button" onClick={handleRegister} >Submit</Button> }
 
